132302fd636d6bd336f5a6be21cb1e4759b24f0e
mailahnheidi@gmail.com <mailahnheidi@gmail.com>
2021-12-03 15:45:51 -0600

    started fixing shop-input

--- assets/js/game.js
+++ assets/js/game.js
@@ -162,25 +162,19 @@ var shop = function() {
   );
 
   // use switch case to carry out action
+  debugger;
   switch (shopOptionPrompt) {
-    case 'REFILL':
-    case 'refill':
+    case 1:
       playerInfo.refillHealth();
       break;
-    case 'UPGRADE':
-    case 'upgrade':
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case 'LEAVE':
-    case 'leave':
+    case 3:
       window.alert('Leaving the store.');
-
-      // do nothing, so function will end
       break;
     default:
       window.alert('You did not pick a valid option. Try again.');
-
-      // call shop() again to force player to pick a valid option
       shop();
       break;
   }
